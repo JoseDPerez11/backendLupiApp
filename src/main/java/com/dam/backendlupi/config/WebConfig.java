bbbe7c5e7ed63f60102a53d0229f4eac50e94ae9
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2023-12-15 19:52:40 -0500

    agregando webConfig para personalizar los cors

---
+++ src/main/java/com/dam/backendlupi/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.dam.backendlupi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+* esta clase de configuracion está habilitando y personalizando la configuración de
+* CORS(Cross-Origin Resource Sharing) para permitir el acceso a los recursos desde
+* cualquier origen, con ciertos métodos HTTP permitidos y la capacidad de incluir
+* credenciales en las solicitudes CORS. */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
