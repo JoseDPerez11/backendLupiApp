f11d37f356300ed8ef620168bafaefdbf33e728d
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-23 16:56:45 -0500

    first commit

---
+++ src/main/java/com/dsw/backendlupi/BackendlupiApplication.java
@@ -0,0 +1,13 @@
+package com.dsw.backendlupi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendlupiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendlupiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsw/backendlupi/BackendlupiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsw.backendlupi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendlupiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
