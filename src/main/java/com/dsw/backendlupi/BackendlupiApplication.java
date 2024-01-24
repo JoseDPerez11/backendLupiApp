bc702ec7e37e39e37883ac3a0f92b7b86feb86a7
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-24 13:22:08 -0500

    adding annotation to load properties of FileStorageProperties

--- src/main/java/com/dsw/backendlupi/BackendlupiApplication.java
+++ src/main/java/com/dsw/backendlupi/BackendlupiApplication.java
@@ -1,8 +1,10 @@
 package com.dsw.backendlupi;
 
+import com.dsw.backendlupi.config.FileStorageProperties;
 import com.dsw.backendlupi.exception.FileStorageException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class}) // carga propiedades para almacenamiento de archivos
 public class BackendlupiApplication {
 
 	public static void main(String[] args) {
