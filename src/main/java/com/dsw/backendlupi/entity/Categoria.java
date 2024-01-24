a171ac278a700850994daf0cadbd4e654598ff03
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-23 19:00:09 -0500

    adding class Categoria

--- src/main/java/com/dsw/backendlupi/entity/Categoria.java
+++ src/main/java/com/dsw/backendlupi/entity/Categoria.java
@@ -1,4 +1,21 @@
 package com.dsw.backendlupi.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100)
+    private String nombre;
+    @Column
+    private boolean vigencia;
+    @OneToOne
+    private DocumentoAlmacenado foto;
 }
