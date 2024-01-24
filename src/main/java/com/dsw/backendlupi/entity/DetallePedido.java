217b480f314d8f250497a8c7972733eb4cc349f7
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-23 23:59:31 -0500

    adding detallePedido class

--- src/main/java/com/dsw/backendlupi/entity/DetallePedido.java
+++ src/main/java/com/dsw/backendlupi/entity/DetallePedido.java
@@ -1,4 +1,32 @@
 package com.dsw.backendlupi.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class DetallePedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private int cantidad;
+    @Column
+    private Double precio;
+
+    @ManyToOne
+    private Producto producto;
+    @ManyToOne
+    private Pedido pedido;
+
+    public String getNombre(){
+        return this.producto!= null ? this.producto.getNombre() : "----";
+    }
+
+    public Double getSubTotal(){
+        return this.cantidad * this.precio;
+    }
 }
