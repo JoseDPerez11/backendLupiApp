12c76bb94965d0c5f1502aa2c6fcd4356fe410f8
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-23 23:40:24 -0500

    adding cliente class

--- src/main/java/com/dsw/backendlupi/entity/Cliente.java
+++ src/main/java/com/dsw/backendlupi/entity/Cliente.java
@@ -1,4 +1,43 @@
 package com.dsw.backendlupi.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100)
+    private String nombres;
+    @Column(length = 100)   
+    private String apellidoPaterno;
+    @Column(length = 100)
+    private String apellidoMaterno;
+    @Column(length = 20)
+    private String tipoDoc;
+    @Column(length = 8)
+    private String numDoc;
+    @Column(length = 100)
+    private String direccionEnvio;
+    @Column(length = 100)
+    private String departamento;
+    @Column(length = 100)
+    private String provincia;
+    @Column(length = 100)
+    private String distrito;
+    private String telefono;
+
+    @OneToOne
+    private DocumentoAlmacenado foto;
+
+    public String getNombreCompletoCiente(){
+        return this.nombres != null && this.apellidoPaterno != null && this.apellidoMaterno != null ?
+                this.nombres + " " + this.apellidoPaterno + " " + this.apellidoMaterno: "-----";
+    }
 }
