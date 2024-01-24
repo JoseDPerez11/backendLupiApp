cdc327a1a921b4dc0242b8e4a6b88e167bc0b36d
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-23 23:53:03 -0500

    adding producto class

--- src/main/java/com/dsw/backendlupi/entity/Producto.java
+++ src/main/java/com/dsw/backendlupi/entity/Producto.java
@@ -1,4 +1,36 @@
 package com.dsw.backendlupi.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100)
+    private String nombre;
+
+    @OneToOne
+    private DocumentoAlmacenado foto;
+
+    @Column
+    private Double precio;
+    @Column
+    private int stock;
+    @Column(length = 500)
+    private String descripcionProducto;
+
+    @OneToOne
+    private Categoria categoria;
+    
+    @Column
+    private boolean vigencia;
+    @Column
+    private boolean recomendado;
 }
