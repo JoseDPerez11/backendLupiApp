9467c379804015eb64235837aa93148663d5ba13
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-01-27 15:57:44 -0500

    creating UserController for login

--- src/main/java/com/dsw/backendlupi/controller/UsuarioController.java
+++ src/main/java/com/dsw/backendlupi/controller/UsuarioController.java
@@ -1,2 +1,27 @@
-package com.dsw.backendlupi.controller;public class UsuarioController {
+package com.dsw.backendlupi.controller;
+
+import com.dsw.backendlupi.entity.Usuario;
+import com.dsw.backendlupi.service.UsuarioService;
+import com.dsw.backendlupi.utils.GenericResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("api/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService uService;
+
+    @PostMapping("/login")
+    public GenericResponse<Usuario> login(HttpServletRequest request) {
+        String email = request.getParameter("email");
+        String contrasenia = request.getParameter("pass");
+        return uService.login(email, contrasenia);
+    }
+
 }
